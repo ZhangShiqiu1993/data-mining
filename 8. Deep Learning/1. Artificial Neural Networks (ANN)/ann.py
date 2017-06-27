0ad6bf53d891dc3dbf2af256804d13b3ac7645a5
zhangshiqiu <zhangshiqiu1993@126.com>
2017-06-26 21:39:19 -0700

    ANN

--- 8. Deep Learning/1. Artificial Neural Networks (ANN)/ann.py
+++ 8. Deep Learning/1. Artificial Neural Networks (ANN)/ann.py
@@ -24,17 +24,42 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score, GridSearchCV
 from keras.models import Sequential
-from keras.layers import Dense
-classifier = Sequential()
-classifier.add(Dense(kernel_initializer='uniform', activation = 'relu', input_dim = 11, units=6))
-classifier.add(Dense(kernel_initializer = 'uniform', activation = 'relu', units=6))
-classifier.add(Dense(kernel_initializer = 'uniform', activation = 'sigmoid', units=1))
-classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
-classifier.fit(X_train, y_train, batch_size = 10, epochs=100)
+from keras.layers import Dense, Dropout
 
-y_pred = classifier.predict(X_test)
-y_pred = (y_pred > 0.5)
+def build_classifier(optimizer = 'adam'):
+    classifier = Sequential()
+    classifier.add(Dense(kernel_initializer='uniform', activation = 'relu', input_dim = 11, units=6))
+    classifier.add(Dropout(rate=0.1))
+    classifier.add(Dense(kernel_initializer = 'uniform', activation = 'relu', units=6))
+    classifier.add(Dropout(rate=0.1))
+    classifier.add(Dense(kernel_initializer = 'uniform', activation = 'sigmoid', units=1))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
 
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+def run_K_fold_cross_validation(k=10):
+    classifier = KerasClassifier(build_fn=build_classifier, batch_size=16, epochs=128)
+    accuracies = cross_val_score(estimator=classifier, X=X_train, y=y_train, cv=10, n_jobs=-1)
+    mean = accuracies.mean()
+    variance = accuracies.std()
+    print("mean : " + mean)
+    print("std : " + variance)
+
+def run_grid_search():
+    classifier = KerasClassifier(build_fn=build_classifier)
+    parameters = {'batch_size' : [16, 25, 32],
+                  'epochs': [32, 64, 128, 256, 512],
+                  'optimizer' : ['adam', 'rmsprop']}
+    grid_search = GridSearchCV(estimator=classifier, 
+                               param_grid=parameters,
+                               scoring='accuracy',
+                               cv=10)
+    grid_search = grid_search.fit(X_train, y_train)
+    best_parameters = grid_search.best_params_
+    best_accuracy = grid_search.best_score_
+    print("best parameters : " + best_parameters)
+    print("best accuracy : " + best_accuracy)
+    
+run_grid_search()
